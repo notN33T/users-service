b79fc6db81eaa43fc973898b178384910c35c4d6
Evgeny Trubish <evgeny.trubish@effective-soft.com>
2022-07-09 14:12:47 +0300

    Init user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UserModule],
 })
 export class AppModule {}
